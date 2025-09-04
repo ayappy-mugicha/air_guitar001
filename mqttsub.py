27004a1aac467858d5fcc130bf58821e3b0d7cd9
ayappy-mugicha <ayatosync13@gmail.com>
2025-09-04 09:07:52 +0900

    トピックを入力させるようにした

--- mqttsub.py
+++ mqttsub.py
@@ -1,26 +1,16 @@
 from mqttsys import host, port, connect_mqtt
 from share_queue import msg_queue # キューをインポート
 topic = 'guitar/stroke'
-# payload = []
-payload = None
 
 def on_message(client, userdata, msg):
-    # print(f"{msg.topic} : {msg.payload.decode()}" )
-    # payload.append(msg.payload.decode())
+    print(f"{msg.topic} : {msg.payload.decode()}" ) # 受信したメッセージを表示
     msg_queue.put(msg.payload.decode()) # キューにメッセージを追加
     
-# def get_msg():
-#     if payload:
-#         # return payload.pop(0)
-#         return payload
-        
-    # return None
-
-def mqtt_init():
-    client = connect_mqtt(host,port)
-    client.on_message = on_message
-    client.subscribe(topic)
-    client.loop_start()
+def mqtt_init(topic):
+    client = connect_mqtt(host,port) # MQTTブローカーに接続
+    client.on_message = on_message # メッセージ受信時のコールバックを設定
+    client.subscribe(topic) # トピックを購読
+    client.loop_start() # 非同期でメッセージを処理するループを開始
     return client
 
 def stop_mqtt(client):
@@ -29,12 +19,9 @@ def stop_mqtt(client):
     print("mqtt_bye!!")
 
 
-def run():
+def run(topoic=topic):
     print("this is mqttsub")
-    client = mqtt_init()
-    # msg = get_msg()
-    # print(msg)
-    # client.loop_forever()
+    client = mqtt_init(topic)
     
 if __name__ == "__main__":
     run() 
