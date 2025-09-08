27f10bed613de43ef9c1e8a70bad593214e8b57a
ayappy-mugicha <ayatosync13@gmail.com>
2025-09-08 15:31:23 +0900

    コメントを追加する

--- sendcode.py
+++ sendcode.py
@@ -3,7 +3,7 @@ import mqttpub
 topic = "guitar/code"
 client = mqttpub.publish_init()
 def send(message):
-    mqttpub.publish(client, topic, message)
+    mqttpub.publish(client, topic, message) # mqttpubに送信
     
 if __name__ == "__main__":
     message = "c"
