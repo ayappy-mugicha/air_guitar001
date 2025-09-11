fafba87b24ee3bb0f6442340cd23d593c186208a
ayappy-mugicha <ayatosync13@gmail.com>
2025-09-12 00:59:23 +0900

    コメントを追加と、プログラムをもっと厳密にした

--- chordcrl_PCA9685.c
+++ chordcrl_PCA9685.c
@@ -28,6 +28,8 @@
 
 // グローバル変数
 int fd;
+
+// サーボのチャンネル
 int C = 0;
 int D = 1;
 int E = 2;
@@ -39,11 +41,20 @@ int Am = 7;
 int Dm = 8;
 int Bm = 9;
 
+// コードの数と配置（ほとんど使ってない）
 char *chordlist[] = {"C", "D", "E", "F", "G", "A", "Em", "Am", "Dm", "Bm"};
 
 // サーボの角度設定
-#define OPEN_ANGLE 0   // 開放弦の角度（仮）
-#define PRESS_ANGLE 90 // 押弦の角度（仮）
+#define OPEN_ANGLE 90   // 開放弦の角度（仮）
+#define LEFT_ANGLE 0 // 押弦の角度（仮）
+#define RIGHT_ANGLE 180 // 押弦の角度
+
+/* コンパイルするときこれ使ってね
+
+gcc -o codecrl_PCA9685 chordcrl_PCA9685.c -l wiringPi -FPIC
+gcc -shared -o libchordcrl_PCA9685.so chordcrl_PCA9685.c -l wiringPi -fPIC
+
+*/
 
 // 既存の関数を再掲（一部調整）
 void setPWMFreq(int freq) {
@@ -62,6 +73,7 @@ void setPWMFreq(int freq) {
     wiringPiI2CWriteReg8(fd, MODE1, oldmode | 0xa1);
 }
 
+// サーボを動かす
 void setPWM(int channel, int on, int off) {
     wiringPiI2CWriteReg8(fd, LED0_ON_L + 4 * channel, on & 0xFF);
     wiringPiI2CWriteReg8(fd, LED0_ON_H + 4 * channel, on >> 8);
@@ -69,55 +81,77 @@ void setPWM(int channel, int on, int off) {
     wiringPiI2CWriteReg8(fd, (LED0_ON_H + 4 * channel) + 2, off >> 8); // LED0_OFF_H
 }
 
+// サーボの角度をPWMに変換
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+// サーボをすべてオフにする（開放弦）
 void openPWM(int channel) {
+    
     int pulse_width = map(OPEN_ANGLE, 0, 180, SERVO_MIN, SERVO_MAX);
     setPWM(channel, 0, pulse_width);
     printf("Channel %d: Open\n", channel);
 }
 
+// 開放弦にする
 void allopen() {
+    printf("all open\n");
     // 全てのチャンネルを開放状態にする
     for (int i = 0; i < 10; i++) { // ループ回数を10に修正
         openPWM(i);
     }
-    delay(1000);
+    // delay(1000);
 }
 
-void presschord(int chord_channel) {
+// コードを押す
+void presschord(int chord_channel , int howangle) {
     
     printf("----------%s------------\n",chordlist[chord_channel]);
     // まず全てのチャンネルを開放状態にする
     allopen();
     // 指定されたチャンネルを押弦状態にする
-
-    int pulse_width = map(PRESS_ANGLE, 0, 180, SERVO_MIN, SERVO_MAX); // サーボの角度を計算してもらう
-    setPWM(chord_channel, 0, pulse_width); // PWMでサーボを動かす
-    printf("Pressed chord: %s (Channel %d) at angle %d\n", chordlist[chord_channel], chord_channel, PRESS_ANGLE);
+    if (0 == howangle){ // 左
+        
+        int pulse_width = map(LEFT_ANGLE, 0, 180, SERVO_MIN, SERVO_MAX); // サーボの角度を計算してもらう
+        setPWM(chord_channel, 0, pulse_width); // PWMでサーボを動かす
+        printf("Pressed chord: %s (Channel %d) at angle %d\n", chordlist[chord_channel], chord_channel, LEFT_ANGLE);
     
-    delay(1000);
+    }else { // 右
+    
+        int pulse_width = map(RIGHT_ANGLE, 0, 180, SERVO_MIN, SERVO_MAX); // サーボの角度を計算してもらう
+        setPWM(chord_channel, 0, pulse_width); // PWMでサーボを動かす
+        printf("Pressed chord: %s (Channel %d) at angle %d\n", chordlist[chord_channel], chord_channel, RIGHT_ANGLE);
+    
+    }
+    
+    // delay(1000);
 }
 
-int main(void) {
+int setup(void){
     // I2Cセットアップ
-    if ((fd = wiringPiI2CSetup(PCA9685_ADDRESS)) < 0) {
+    if ((fd = wiringPiI2CSetup(PCA9685_ADDRESS)) < 0) { // i2cが見つからなかったら
         printf("I2C setup failed. Make sure wiringPi is installed and i2c is enabled.\n");
         return 1;
     }
     printf("I2C setup successful at address 0x%02x.\n", PCA9685_ADDRESS);
+    delay(1000);
 
     // PWM周波数設定
     setPWMFreq(SERVO_FREQ);
 
     // 全てのサーボを開放状態にする
     allopen();
+    return 0;
+}
 
+int main(void) { // テスト環境（モジュールで使うので、この関数は使わない）
+
+    setup();
     // 全てのコードを順番に試す
     for (int i = 0; i < 10; i++) {
-        presschord(i);
+        presschord(i,0);
+        presschord(i,1);
     }
 
     return 0;
