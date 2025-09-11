7ab09f0506a750b2d4141c905b947c2ee94b5aff
ayappy-mugicha <ayatosync13@gmail.com>
2025-09-12 01:00:34 +0900

    その他

--- testpub.py
+++ testpub.py
@@ -1,12 +1,12 @@
 import mqttpub
 import time
 import random
-# topic = "guitar/stroke"
-topic = "guitar/code"
+topic = "guitar/stroke"
+# topic = "guitar/code"
 codes = ["a","c"]
 client = mqttpub.publish_init()
 while True:
     val = random.randint(0,1)
-    mqttpub.publish(client,topic,codes[val])
-    # mqttpub.publish("0")
+    # mqttpub.publish(client,topic,codes[val])
+    mqttpub.publish(client,topic,val)
     time.sleep(2)
